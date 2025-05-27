3fc9365317b1dfc7a94f2f5b8a5541984ab85358
Yury Belokhonov <belokhonov@gmail.com>
2025-05-27 14:10:20 +0300

    Add Slices, Length and Capacity (len & cap)

--- go-arrays/main.go
+++ go-arrays/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// Объявление массива из 3-элементов
@@ -14,15 +16,45 @@ func main() {
 	transactionsB := [3]int{} // [0 0 0] Работает только внутри функций
 
 	// Инициализировать с предопределённым набором элементов
-	transactions := [3]int{5, 10, -7}
+	transactions := [6]int{1, 2, 3, 4, 5, 6}
 	banks := [2]string{"Тинькофф", "Альфа"}
 	wallets := [2]string{} // Объявить как переменную без присвоения
 	wallets[0] = "Черный"  // Записать определенный элемент по индексу
 
 	fmt.Println(transactionsA)   // [0 0 0]
 	fmt.Println(transactionsB)   // [0 0 0]
-	fmt.Println(transactions)    // [5 10 -7]
-	fmt.Println(transactions[1]) // 10 Прочитать определенный элемент по индексу
+	fmt.Println(transactions)    // [1 2 3 4 5 6]
+	fmt.Println(transactions[1]) // 2 Прочитать определенный элемент по индексу
 	fmt.Println(banks)           // [Тинькофф Альфа]
 	fmt.Println(wallets)         // [Черный ]
+
+	copy := transactions            // Это полная независимая копия массива
+	copy[1] = 100                   // Изменение не повлияет на исходный массив
+	fmt.Println(transactions, copy) // [1 2 3 4 5 6] [1 100 3 4 5 6]
+
+	// Slice
+	patrial := transactions[1:] // Часть от 1 до последнего
+	fmt.Println(patrial)        // [2 3 4 5 6]
+
+	// Slice ссылается на исходный массив
+	// Любое изменение slice приводит к изменению исходного массива
+	transactionsPartial := transactions[1:5]          // slice от array
+	fmt.Println(transactionsPartial)                  // [2 3 4 5]
+	transactionsNewPartial := transactionsPartial[:1] // slice от slice
+	fmt.Println(transactionsNewPartial)               // [2]
+	transactionsNewPartial[0] = 30                    // изменит slice и array
+
+	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial))
+
+	transactionsNewPartial = transactionsNewPartial[0:4] // это уже другой slice
+
+	fmt.Println(transactions)           // [1 30 3 4 5 6]
+	fmt.Println(transactionsNewPartial) // [30 3 4 5] часть массива transactions
+
+	// Length (Длина): Актуальное количество элементов в слайсе.
+	// Capacity (Вместимость): Максимальное количество элементов,
+	// которое слайс может содержать, основываясь на размере исходного массива.
+
+	fmt.Println(len(transactionsPartial), cap(transactionsPartial))       // 4 5
+	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial)) // 4 5
 }
