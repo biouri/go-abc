e7c1eb80af7aeb270ec4cac82c58bea232a9f9a2
Yury Belokhonov <belokhonov@gmail.com>
2025-05-30 17:38:49 +0300

    Add for Loop on Arrays + Calculate Balance Sum Example

--- go-arrays/main.go
+++ go-arrays/main.go
@@ -5,12 +5,17 @@ import "fmt"
 // В цикле спрашиваем ввод транзакций: -10, 10, 40.5
 // Добавлять каждую в массив транзакций
 // Вывести массив
+// !Вывести сумму баланса в консоль
 
 func main() {
 	tr1 := []int{1, 2, 3}
 	tr2 := []int{4, 5, 6}
 	tr1 = append(tr1, tr2...)
 	fmt.Println(tr1)
+
+	for index, value := range tr1 {
+		fmt.Println(index, value)
+	}
 
 	transactions := []float64{}
 	for {
@@ -21,6 +26,9 @@ func main() {
 		transactions = append(transactions, transaction)
 	}
 	fmt.Println(transactions)
+
+	balance := calculateBalance(transactions)
+	fmt.Printf("Ваш баланс: %.2f", balance)
 }
 
 func scanTransaction() float64 {
@@ -28,4 +36,12 @@ func scanTransaction() float64 {
 	fmt.Print("Введите транзакцию (n для выхода): ")
 	fmt.Scan(&transaction)
 	return transaction
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance += value
+	}
+	return balance
 }
