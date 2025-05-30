39874745e51150cabcfecea2a313d68aabb80874
Yury Belokhonov <belokhonov@gmail.com>
2025-05-30 19:35:32 +0300

    Add Make Memory Allocation for Arrays + Principle of Capacity

--- go-arrays/main.go
+++ go-arrays/main.go
@@ -17,6 +17,19 @@ func main() {
 		fmt.Println(index, value)
 	}
 
+	// make([]ТипЭлемента, длина, ёмкость)
+	// По умолчанию, если указать длину >0 будет создан массив с нулевым элементом ""
+	// Итоговый массив будет иметь нулевой элемент [  1 2 3]
+	tr := make([]string, 0, 2)
+	fmt.Println(len(tr), cap(tr)) // 0 2
+	tr = append(tr, "1")
+	fmt.Println(len(tr), cap(tr)) // 1 2
+	tr = append(tr, "2")
+	fmt.Println(len(tr), cap(tr)) // 2 2
+	tr = append(tr, "3")
+	fmt.Println(len(tr), cap(tr)) // 3 4
+	fmt.Println(tr)
+
 	transactions := []float64{}
 	for {
 		transaction := scanTransaction()
