45344337c3c309f50ae431f3d07423d86bdecbe6
Yury Belokhonov <belokhonov@gmail.com>
2025-06-01 14:44:52 +0300

    Add Using the Function with a Pointer

--- go-password/main.go
+++ go-password/main.go
@@ -6,11 +6,15 @@ func main() {
 	a := 5
 	// var pointerA *int - это указатель на переменную int
 	pointerA := &a // Создание указателя на переменную a
-	res := double(a)
-	fmt.Println(pointerA) // 0xc00000a0e8
-	fmt.Println(res)      // 10
+	// Оператор * dereference: значение по адресу памяти
+	fmt.Println(*pointerA) // 5
+	double(&a)
+	fmt.Println(a) // 10
 }
 
-func double(num int) int {
-	return num * 2
+// Функция double принимает ссылку на int и ничего не возвращает
+// В результате происходит мутирование (изменение) исходного объекта
+func double(num *int) {
+	// Мутирование исходного объекта
+	*num = *num * 2 // *num - * получить значение из адреса в памяти
 }
