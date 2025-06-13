9788ae5545673082321d420b36d18e6d2b9ffd2c
Yury Belokhonov <belokhonov@gmail.com>
2025-06-13 16:54:49 +0300

    Add Selection Menu

--- go-password/main.go
+++ go-password/main.go
@@ -9,9 +9,49 @@ import (
 func main() {
 	files.ReadFile("file.txt")
 
-	createAccount()
+	// 1. Создать аккаунт
+	// 2. Найти аккаунт
+	// 3. Удалить аккаунт
+	// 4. Выход
+	fmt.Println("__Менеджер паролей__")
+Menu:
+	for {
+		variant := getMenu()
+		switch variant {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu // Выход из меню
+		}
+	}
 }
 
+func getMenu() int {
+	var variant int
+	fmt.Println("Выберите вариант:")
+	fmt.Println("1. Создать аккаунт")
+	fmt.Println("2. Найти аккаунт")
+	fmt.Println("3. Удалить аккаунт")
+	fmt.Println("4. Выход")
+	fmt.Scanln(&variant)
+	return variant
+}
+
+// Mock - функция для поиска Account
+func findAccount() {
+
+}
+
+// Mock - функция для удаления Account
+func deleteAccount() {
+
+}
+
+// Создать аккаунт
 func createAccount() {
 	// Запрос данных пользователя
 	login := promptData("Введите логин")
